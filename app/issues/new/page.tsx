aa717e386cfe626f8b8eb07a5c1626dde69d742a
AlexanderNuar <101461783+AlexanderNuar@users.noreply.github.com>
2024-01-20 18:02:11 +0300

    add loading skeletons

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -31,6 +31,17 @@ const NewIssuePage = () => {
   const [error, setError] = useState('')
   const [isSubmitting, setIsSubmitting] = useState(false)
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setIsSubmitting(true)
+      await axios.post('/api/issues', data)
+      router.push('/issues')
+    } catch (error) {
+      setIsSubmitting(false)
+      setError('An unexpected error occurred.')
+    }
+  })
+
   return (
     <div className="max-w-xl">
       {error && (
@@ -38,19 +49,7 @@ const NewIssuePage = () => {
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
-      <form
-        className="space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setIsSubmitting(true)
-            await axios.post('/api/issues', data)
-            router.push('/issues')
-          } catch (error) {
-            setIsSubmitting(false)
-            setError('Unexpected error occurred.')
-          }
-        })}
-      >
+      <form className="space-y-3" onSubmit={onSubmit}>
         <TextField.Root>
           <TextField.Input placeholder="Tittle" {...register('title')} />
         </TextField.Root>

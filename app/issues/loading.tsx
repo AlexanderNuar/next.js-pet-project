e36b5a7c26d57b889c90b6b5fa8632c2953ab4a0
AlexanderNuar <101461783+AlexanderNuar@users.noreply.github.com>
2024-01-21 11:35:37 +0300

    add edit btn

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,6 +1,5 @@
-import 'react-loading-skeleton/dist/skeleton.css'
+import { Skeleton } from '@/app/components'
 import { Table } from '@radix-ui/themes'
-import Skeleton from 'react-loading-skeleton'
 import IssueActions from './IssueActions'
 
 const LoadingIssuesPage = () => {
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,14 @@
+import { Skeleton } from '@/app/components'
+import { Box } from '@radix-ui/themes'
 import React from 'react'
 
 const LoadingNewIssuePage = () => {
-  return <div>Loading...</div>
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Skeleton height="20rem" />
+    </Box>
+  )
 }
 
 export default LoadingNewIssuePage

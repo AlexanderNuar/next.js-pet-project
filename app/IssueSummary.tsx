57f4f547cb83908690387f6e3d6400420f8814d6
AlexanderNuar <101461783+AlexanderNuar@users.noreply.github.com>
2024-01-24 11:55:15 +0300

    issue chart

---
+++ app/IssueSummary.tsx
@@ -0,0 +1,46 @@
+import { Status } from '@prisma/client'
+import { Card, Flex, Text } from '@radix-ui/themes'
+import Link from 'next/link'
+import React from 'react'
+
+type Statuses = {
+  label: string
+  value: number
+  status: Status
+}[]
+
+interface IssueSummaryProps {
+  open: number
+  inProgress: number
+  closed: number
+}
+
+const IssueSummary = ({ open, closed, inProgress }: IssueSummaryProps) => {
+  const containers: Statuses = [
+    { label: 'Open Issues', value: open, status: 'OPEN' },
+    { label: 'In-progress', value: inProgress, status: 'IN_PROGRESS' },
+    { label: 'Closed', value: closed, status: 'CLOSED' },
+  ]
+
+  return (
+    <Flex gap="4">
+      {containers.map((container) => (
+        <Card key={container.label}>
+          <Flex direction="column" gap="1">
+            <Link
+              className="text-sm font-medium"
+              href={`/issues/list?status=${container.status}`}
+            >
+              {container.label}
+            </Link>
+            <Text size="5" className="font-bold">
+              {container.value}
+            </Text>
+          </Flex>
+        </Card>
+      ))}
+    </Flex>
+  )
+}
+
+export default IssueSummary
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+import IssueChart from './IssueChart'
+import IssueSummary from './IssueSummary'
 import LatestIssues from './LatestIssues'
 import Pagination from './components/Pagination'
+import prisma from '@/prisma/client'
 
-export default function Home() {
-  return <LatestIssues />
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: 'OPEN' },
+  })
+  const inProgress = await prisma.issue.count({
+    where: { status: 'IN_PROGRESS' },
+  })
+  const closed = await prisma.issue.count({
+    where: { status: 'CLOSED' },
+  })
+
+  return <IssueChart open={open} inProgress={inProgress} closed={closed} />
 }
